e2ae65309b7ec5f6b88c5e8b7aa57bd78bc0d360
Lauren <laurenkersten@gmail.com>
2021-05-31 17:46:35 +0200

    File selection on os works now

--- src/plot_ticker.py
+++ src/plot_ticker.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+from pathlib import Path
 
-def plot_daily_close(ticker_name: str) -> None:
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+def plot_daily_close(ticker_name, cwd):
+
     # Reads Data and selects Date, Open, High, Low, Close and Volume columns for the Pandas dataframe
-    dataset = pd.read_csv('data\\' + ticker_name + ".csv", usecols = [0,1,2,3,4,6])
+    dataset = pd.read_csv(r'%s' %cwd + os.path.sep + ticker_name + ".csv", usecols = [0,1,2,3,4,6])
 
     # Plots the Daily Prices of Selected Ticker
     plt.plot(dataset[['Close']], 'g', label = 'Closing price')
@@ -12,6 +18,11 @@ def plot_daily_close(ticker_name: str) -> None:
     plt.show()
 
 
-if __name__ == '__main__':
-    ticker_name = input("Ticker name: ")
-    plot_daily_close(ticker_name)
+if __name__=='__main__':
+	ticker_name = input("Ticker name: ")
+	cwd = os.getcwd()
+	cwd = os.path.split(cwd)
+	cwd = os.path.join(cwd[0], "data")
+
+	plot_daily_close(ticker_name, cwd)
+
